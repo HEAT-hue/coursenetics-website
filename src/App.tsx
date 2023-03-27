28c58397e393f9c23a3b1406b2612c19982e6dc0
mightGuy <onyejemeemmanuel65@gmail.com>
2023-03-27 02:09:23 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl text-blue-800 font-bold underline">
+        Welcome to Coursenetics LMS
+      </h1>
+    </div>
+  )
+}
