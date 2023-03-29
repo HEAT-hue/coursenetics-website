80ccad3ff2ec3253630fd54d37d4e7579e625e76
Daniel <ifeoluwadaniel25@gmail.com>
2023-03-29 07:44:29 +0100

    Allowed JS

--- src/pages/home/subroutes/Home.tsx
+++ src/pages/home/subroutes/Home.tsx
@@ -1,10 +1,13 @@
 // jshint esversion:6
+import Navbar from "../../../components/navbar/navbar";
 
 function HomePage() {
     return (
         <>
+        <Navbar/>
             <div>
-                This is the main home page
+                
+                This is the main home page right?
             </div>
         </>
     );
